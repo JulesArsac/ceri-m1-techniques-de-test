4950995bd5212625455bcad3e587ab29e90f2a82
JulesArsac <JulesArsac@users.noreply.github.com>
2024-04-07 01:16:15 +0000

    deploy: dfba263095f7d592524b29197e22dc5c35b14bed

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++
@@ -1,9 +0,0 @@
-package fr.univavignon.pokedex.api;
-
-public class PokemonTrainerFactory {
-
-    public static PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-        return new PokemonTrainer(name, team, pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory()));
-    }
-
-}
